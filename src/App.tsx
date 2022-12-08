c3d7a3ef245c1ba84c9c23734a5118d31c246c38
Ori Baram <obrm770@gmail.com>
2022-12-08 13:07:24 +0200

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import { TodoList, TodoInput } from './components';
 
-import TodoStore from './stores/TodoStore';
-
-const todos = new TodoStore();
-
 const App = () => {
   return (
     <div className="app">
