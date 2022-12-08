bd6130b6926cf8ede99a787dd7ffa05866a68ba0
Ori Baram <obrm770@gmail.com>
2022-12-08 15:07:59 +0200

    useLocalObservable

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { observer, useLocalObservable } from 'mobx-react-lite';
+
 import { TodoList, TodoInput } from './components';
 
+import styles from './styles/App.module.css';
+
 const App = () => {
+  // Use of useLocalObservable instead of useState (for complex local state)
+  const appUI = useLocalObservable(() => ({
+    todosVisible: true,
+    toggleTodoVisibility() {
+      appUI.todosVisible = !appUI.todosVisible;
+    },
+  }));
+
+  const handleClick = () => appUI.toggleTodoVisibility();
+
   return (
     <div className="app">
       <TodoInput />
-      <TodoList />
+      <div className={styles["todo-list-wrapper"]}>
+        <h2 onClick={handleClick}>
+          <span>{appUI.todosVisible ? "-" : "+"}</span>
+          Todos</h2>
+        {appUI.todosVisible && <TodoList />}
+      </div>
     </div>
   );
 };
 
-export default App;
+export default observer(App);
